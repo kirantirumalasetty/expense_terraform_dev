6bb423a97030c93eed2d333a6ed4e05017446d23
kiran <kirantwls@gmail.com>
2024-12-19 19:33:55 +0530

    expense terraform dev

--- 30-bastion/provider.tf
+++ 30-bastion/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket = "81s-remote-state"
-    key    = "expense-sg"
+    key    = "expense-bastion"
     region = "us-east-1"
     dynamodb_table = "81s-locking"
   }
